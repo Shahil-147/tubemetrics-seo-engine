a53fc51dffaa88e29be69c809e2affb87e21c8ad
Shahil-1421 <shahilgupta14012006@gmail.com>
2026-04-11 02:10:09 +0530

    Docker setup

--- YoutubeTools/src/test/java/com/YoutubeTools/YoutubeToolsApplicationTests.java
+++ YoutubeTools/src/test/java/com/YoutubeTools/YoutubeToolsApplicationTests.java
@@ -2,8 +2,10 @@ package com.YoutubeTools;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "youtube.api.key=dummy-key-for-tests")
 class YoutubeToolsApplicationTests {
 
 	@Test
